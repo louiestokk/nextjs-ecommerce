5e8448adedb7b5262981867d2c69516c41666b00
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-06 07:23:36 +0200

    css fix

--- components/Hero.js
+++ components/Hero.js
@@ -33,9 +33,9 @@ const Hero = () => {
   return (
     <div className='hero-container' style={{margin:'0 auto'}}>
     <img src='/assets/images/everything.png' alt='rea smycken' className='hero-main-img'/>
-    <div style={{display:'flex',flexDirection:'row',width:'100%',marginTop:'0.5rem'}}>
-      <img src='/assets/images/orhangen.png' alt='örhängen rea' className='hero-banner-img'/>
-      <img src='/assets/images/ringar.png' alt='ringar rea' className='hero-banner-img'/>
+    <div style={{display:'flex',flexDirection:'row',width:'100%',marginTop:'0.4rem'}}>
+      <img src='/assets/images/orhangen.png' alt='örhängen rea' className='hero-banner-img' style={{ margin:'0 0.25rem'}}/>
+      <img src='/assets/images/ringar.png' alt='ringar rea' className='hero-banner-img' style={{margin:'0 0.25rem'}}/>
     </div>
     </div>
   )
