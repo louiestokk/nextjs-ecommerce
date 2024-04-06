575e2f6911237fee4e9a3ee4970884558cfc2207
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-06 12:07:57 +0200

    fix

--- components/UtvaldaKategorier.js
+++ components/UtvaldaKategorier.js
@@ -3,8 +3,8 @@ import React from 'react'
 const UtvaldaKategorier = ({categories}) => {
   return (
     <>
- <h2 style={{fontWeight:'bold',fontSize:'2rem'}} className='populara-produkter'>Utvalda produkter av .struts </h2>
-<div style={{justifyContent:'center',display:'flex',flexWrap:'wrap',margin:'2rem 0'}}>
+ <h2 style={{fontWeight:'bold',fontSize:'2rem'}} className='populara-produkter'>Utvalda kategorier av .struts </h2>
+<div style={{justifyContent:'center',display:'flex',flexWrap:'wrap',marginBottom:'3rem'}}>
 
 {categories.map((el,i)=>(
     <button key={i} className='utvl-btn'>{el.name}</button>
