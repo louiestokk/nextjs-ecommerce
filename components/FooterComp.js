3d1c3b3011a0ef28acc48ab9fb5df56b935efb39
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-07 13:40:15 +0200

    footer

--- components/FooterComp.js
+++ components/FooterComp.js
@@ -11,12 +11,41 @@ import { footerlinksstore } from '../utils/pagelinks'
 import XIcon from '@mui/icons-material/X';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TelegramIcon from '@mui/icons-material/Telegram';
+import { FaCheck } from "react-icons/fa6";
 import Image from 'next/image';
 const FooterComp = () => {
     let year = new Date().getFullYear()
   return (
     <footer>
     <div>
+    <h2>Ditt online köpcenter med Sveriges bästa priser!</h2>
+    <ul>
+      <li>
+        <FaCheck className='footer-check'/>
+        <span>Kvalitetssäkrade produkter</span>
+      </li>
+      <li>
+        <FaCheck className='footer-check'/>
+        <span>Snabba leveranser</span>
+      </li>
+      <li>
+        <FaCheck className='footer-check'/>
+        <span>Snabb kundservice</span>
+      </li>
+    </ul>
+    <div>
+    <p style={{fontWeight:'bold',marginTop:'1.8rem',marginBottom:'0.5rem'}}>Betalnigsalternativ</p>
+    <div className='betalnings-alt'>
+      <img src='/assets/images/visa.jpg' alt='visa logo' className='payment-logo' />
+      <img src='/assets/images/mastercard.jpg' alt='mastercard logo' className='payment-logo' />
+      <img src='/assets/images/logo_paypal.jpg' alt='paypal logo' className='payment-logo' />
+      <img src='/assets/images/applepay.jpg' alt='paypal logo' className='payment-logo' style={{width:'110px'}}/>
+    </div>
+    <div>
+    <p style={{fontWeight:'bold',marginTop:'1.8rem',marginBottom:'0.5rem'}}>Trygg e-handel</p>
+    <img src='/assets/images/trygg-ehandel.jpg' alt='visa logo' className='payment-logo' style={{marginBottom:'1.4rem'}}/>
+    </div>
+  
         {footerlinksupport.map((el,i)=>(
             <Accordion key={i} className='accordion'>
             <AccordionSummary
@@ -250,10 +279,12 @@ const FooterComp = () => {
 <FacebookIcon className='footer-icons'/>
 <TelegramIcon className='footer-icons'/>
     </div>
-    <div style={{display:'flex',alignItems:'center'}}>
+    <div style={{display:'flex',justifyContent:'center'}}>
     <p className='text-xs'>Copyright <span>&copy;</span> {year} .struts </p>
     <GiOstrich/>
     </div>
+    </div>
+  
  
     </footer>
   )
