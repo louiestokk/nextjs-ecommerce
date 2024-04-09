6b83288e8819c2409a32a5d02f669d7cc47938e5
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-09 12:47:00 +0200

    productDetails

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import { FaStar } from "react-icons/fa6";
 import { BsFire } from "react-icons/bs";
 import { BsTruck } from "react-icons/bs";
@@ -47,6 +48,13 @@ const addProduct = async(productId)=>{
     <p className='text-sm font-bold product-price'>{product.price.formatted_with_symbol}</p>
     <TbShoppingBagPlus style={{color:'rgb(0, 131, 138',fontSize:'1.5rem',cursor:'pointer'}} onClick={()=>addProduct(product.id)}/>
     </div>
+    <Link href={{
+    pathname: `/product/[id]`,
+    query: {
+      title: product.id, // should be `title` not `id`
+    },
+  }}
+  as={`/product/${product.id}`}>go to </Link>
     <span className='rea-procent'>kr{Math.round(product.price.raw*1.3)}</span>
     </div>
     <p className='text-sm product-name'>{product?.name}</p>
