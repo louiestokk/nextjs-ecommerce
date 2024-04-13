53b4c16ba106632c0e937b3526d8ee58878716fd
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-13 13:23:37 +0200

    productPage

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,43 +1,47 @@
+'use client'
+
+import React,{useState,useEffect} from 'react';
 import commerce from '../../../lib/commerce'
 import { FaStar } from "react-icons/fa6";
 
-const getProductDetails = async(productId)=>{
+
+export default function ProductDetails({params}){
+const [itemIndex, setItemIndex] = useState(0)
+const [currentProduct, setCurrentProduct] = useState({})
+const fetchProductDetails =async(productId)=>{
     try {
         const product = await commerce.products.retrieve(productId)
-       return  new Response(JSON.stringify({product}),{
-        status:200
-    })
+        setCurrentProduct(product)
+        console.log(product.assets)
     } catch (error) {
-        return new Response('Failed to fetch productdetails',{status:500})
+        console.log(error)
     }
 }
-
-
-
-export default async function ProductDetails({params}){
-
-const product = await getProductDetails(`${params.id}`)
-const productDetails = await product.json()
-console.log(params.id)
+useEffect(()=>{
+fetchProductDetails(params.id)
+},[])
     return <section>
+   
         <div className='product_item'>
-            <img src={productDetails?.product?.image?.url} alt={productDetails?.product?.name} style={{maxHeight:'294px'}}/>
+            <img src={currentProduct?.assets?.[itemIndex]?.url} alt={currentProduct?.name} style={{maxHeight:'294px'}}/>
             <div className='product_item_options'>
-            <h2>{productDetails?.product?.variant_groups?.[0]?.name}</h2>
-            <div style={{display:'flex',flexWrap:'wrap'}}>
-            {productDetails.product.assets.map((el)=>(
-                <img src={el.url} key={el.id} style={{width:'55px',width:'55px',margin:'0 0.2rem',objectFit:'cover'}}/>
-            ))}
+            <div style={{display:'flex',alignItems:'center'}}>
+            <h2 style={{fontSize:'0.75rem'}}>{currentProduct?.variant_groups?.[0]?.name}:</h2>
+            <h2 style={{marginLeft:'0.2rem',fontSize:'0.75rem',fontWeight:'bold'}} >{currentProduct?.variant_groups?.[0]?.options?.[itemIndex]?.name}</h2>
             </div>
-            <div style={{display:'flex'}}>
-            {productDetails?.product?.variant_groups?.[0]?.options?.map((el)=> el?.name)?.map((el,i)=>(
-            <p key={i} style={{width:'55px',margin:'0 0.2rem',fontSize:'0.85rem'}}>{el}</p>
-           ))}
+           
+            <div style={{display:'flex',flexWrap:'wrap'}}>
+            {currentProduct.assets.map((el,i)=>(
+                <div key={el.id} onClick={()=> setItemIndex(i)} style={{cursor:'pointer'}}>                
+                <img src={el.url}  style={{width:'55px',width:'55px',margin:'0 0.2rem',objectFit:'cover'}}/>
+                </div>
+        
+            ))}
             </div>
             </div>
             <div className='product_item_details'>
-            <p className='text-xs text-center mt-4'>{productDetails?.product?.categories?.[0]?.name}</p>
-            <h1 className='text-sm mt-1 font-bold text-center max-w-80'>{productDetails?.product?.name}</h1>
+            <p className='text-xs text-center mt-4'>{currentProduct?.categories?.[0]?.name}</p>
+            <h1 className='text-sm mt-1 font-bold text-center max-w-80'>{currentProduct?.name}</h1>
             <div className='stars-container mt-3 mb-1 justify-center'>
     <div className='stars-item'>
     <FaStar className='stars'/>  <FaStar className='stars'/>  <FaStar className='stars'/>  <FaStar className='stars'/>  <FaStar className='stars'/> 
@@ -48,11 +52,11 @@ console.log(params.id)
     </div>
     </div>
             </div>
-        <p dangerouslySetInnerHTML={{__html:productDetails?.product?.description}} className='text-xs text-center max-w-80 mb-2'></p>
+        <p dangerouslySetInnerHTML={{__html:currentProduct?.description}} className='text-xs text-center max-w-80 mb-2'></p>
         </div>
         <div style={{display:'flex',alignItems:'center',justifyContent:'center'}} >
-        <p className='text-sm font-bold product-price text-center'>{productDetails?.product?.price?.formatted_with_symbol}</p>
-        <span className='rea-procent' style={{marginTop:'0.6rem',marginLeft:'0.3rem'}}>kr{Math.round(productDetails?.product?.price?.raw*1.3)}</span>
+        <p className='text-sm font-bold product-price text-center'>{currentProduct?.price?.formatted_with_symbol}</p>
+        <span className='rea-procent' style={{marginTop:'0.6rem',marginLeft:'0.3rem'}}>kr{Math.round(currentProduct?.price?.raw*1.3)}</span>
         </div>
     
     </section>
