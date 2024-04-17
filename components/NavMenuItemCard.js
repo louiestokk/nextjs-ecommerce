9061b0d457c1b298acec66364f0398c356a66e8e
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-04-17 22:09:50 +0200

    cart

--- components/NavMenuItemCard.js
+++ components/NavMenuItemCard.js
@@ -2,12 +2,25 @@
 
 import React,{useEffect,useState} from 'react'
 import commerce from '../lib/commerce'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { IconButton } from '@mui/material';
 
 const NavMenuItemCard = ({product}) => {
   const [fetchdProduct, setFetchdProduct] = useState({})
   const [numberItems, setnumberItems] = useState(product?.quantity)
+const [isRemoving, setIsRemoving] = useState(false)
 
-  const handleNumItemsChange =()=>{}
+  const handleNumItemsChange=()=>{}
+
+  const handleDeleteProduct=async(productId)=>{
+    try {
+      setIsRemoving(true)
+      const resp = await commerce.cart.remove(`${productId}`)
+      setIsRemoving(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const fetchProduct =async()=>{
     try {
@@ -26,15 +39,16 @@ fetchProduct()
     <div style={{marginLeft:'1rem',marginTop:'1rem'}}>
 
     <div style={{display:'flex',width:'100%'}}>
-    <img src={product.image.url} alt={product.name} style={{width:'25%',height:'60px',borderRadius:'3px'}}/>
-    <div style={{marginLeft:'0.75rem',lineHeight:'5px'}}>
+    <img src={product.image.url} alt={product.name} style={{maxWidth:'25%',height:'60px',borderRadius:'3px'}}/>
+    <div style={{marginLeft:'0.75rem',lineHeight:'3px'}}>
     <p style={{fontSize:'0.7rem',color:'gray'}}>{fetchdProduct.categories?.[0]?.name}</p>
       <h4 className='text-sm product-name' style={{maxWidth:'200px'}}>{product.name}</h4>
       <div style={{display:'flex',alignItems:'center',justifyContent:'space-between'}}> 
 
-      <div>
+      <div style={{lineHeight:'0px'}}>
       <p className='text-sm font-bold product-price' style={{fontSize:'0.8rem'}}>{product.price.formatted_with_symbol}</p>
       <span className='rea-procent'>kr{Math.round(product.price.raw*1.3)}</span>
+        <DeleteOutlineIcon style={{color:'gray',fontSize:'1rem',marginLeft:'0.2rem',cursor:'pointer'}} onClick={()=> handleDeleteProduct(product.id)}/>
       </div>
     
       <div style={{display:'flex',alignItems:'center',border:'1px solid lightgray',padding:'0.2rem'}}>
