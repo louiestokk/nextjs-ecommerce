edee8867f864c25c5ea28551bd82b32e432923ff
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-05-20 17:47:31 +0200

    bug fix

--- next.config.js
+++ next.config.js
@@ -5,15 +5,39 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ['lh3.googleusercontent.com','cdn.chec.io'],
+    domains: ['lh3.googleusercontent.com', 'cdn.chec.io'],
   },
   webpack(config) {
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
-    }
-    return config
-  }
-}
+    };
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        // Ensuring correct MIME types for Next.js static files
+        source: '/_next/static/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/javascript',
+          },
+        ],
+      },
+      {
+        // Ensuring correct MIME types for CSS files
+        source: '/styles/:path*',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/css',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
