30580d63192bbb8d6083ebc73769ac4fedc9912c
Louie Stokk <76744837+louiestokk@users.noreply.github.com>
2024-05-22 10:54:53 +0200

    fix bug 7

--- components/StripeCheckout.js
+++ components/StripeCheckout.js
@@ -1,7 +1,7 @@
 'use client'
 
 import React,{useEffect,useState} from 'react'
-import { Elements,useStripe,useElements,PaymentElement,LinkAuthenticationElement,userOrderExits } from '@stripe/react-stripe-js'
+import { Elements,useStripe,useElements,PaymentElement,LinkAuthenticationElement } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 import { Button } from '@mui/material'
 import { useSelector } from 'react-redux'
